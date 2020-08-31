b4495e4fc01e761b34195f11811ecd3701511dd5
soheeeeP <tpfj@naver.com>
2020-08-31 22:14:23 +0900

    Create Book, Review model

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book, Review
+from .forms import ReviewForm
 
 # Register your models here.
+admin.site.register(Book)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    form = ReviewForm 
